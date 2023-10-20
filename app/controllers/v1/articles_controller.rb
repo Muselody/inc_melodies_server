589c40e60bfaa7505f7e2bbe62e8061b715b24c9
emanuel caro <darkemlord@gmail.com>
2023-10-20 23:53:17 +0900

    001 adding rubocop

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -1,40 +1,43 @@
-class V1::ArticlesController < ApplicationController
-  include ResponseHandler
-  before_action :set_article, only: %i[ show update destroy ]
+module V1
+  # This controller is used for handle Article Model
+  class ArticlesController < ApplicationController
+    include ResponseHandler
+    before_action :set_article, only: %i[show update destroy]
 
-  def index
-    @articles = Article.all
-  end
-
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      render json: { success: "article successfully created" }, status: :created
-    else
-      render json: @article.errors, status: :unprocessable_entity
+    def index
+      @articles = Article.all
     end
-  end
 
-  def update
-    if @article.update(article_params)
-      render json: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
+    def create
+      @article = Article.new(article_params)
+      if @article.save
+        render json: { success: 'article successfully created' }, status: :created
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
     end
-  end
 
-  def destroy
-    @article.destroy
+    def update
+      if @article.update(article_params)
+        render json: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @article.destroy
       delete_success
-  end
+    end
 
-  private
+    private
 
-  def set_article
-    @article = Article.find(params[:hashid])
-  end
+    def set_article
+      @article = Article.find(params[:hashid])
+    end
 
-  def article_params
-    params.require(:article).permit(:title, :body)
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
   end
 end
