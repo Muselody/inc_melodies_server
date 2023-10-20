b2f37ea8580b3d63b0598725ff3b2a1dfdc50095
emanuel caro <darkemlord@gmail.com>
2023-10-20 09:09:24 +0900

    refs #001 creating bash to refresh db and adding hashid

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", "~> 5.0"
 
 gem 'rspec-rails', '~> 5.0'
 
+gem "hashid-rails", "~> 1.0"
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show update destroy ]
+
   def index
     @articles = Article.all
     render json: @articles
@@ -29,7 +30,7 @@ class ArticlesController < ApplicationController
   private
 
   def set_article
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:hashid])
   end
 
   def article_params
