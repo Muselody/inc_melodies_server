0f16903bb74619e3c54eeac62f00c02eb1d2c3c5
emanuel caro <darkemlord@gmail.com>
2023-10-24 19:56:53 +0900

    refs updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   scope :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
-      resources :articles, param: :hashid
+      resources :articles, param: :hashid, format: 'json'
     end
   end
   # Defines the root path route ("/")
