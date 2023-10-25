306860f4172a06daf9f90170a8eac74798116dae
emanuel caro <darkemlord@gmail.com>
2023-10-25 20:41:03 +0900

    deleting controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   scope :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
       resources :articles, param: :hashid, format: 'json'
     end
   end
